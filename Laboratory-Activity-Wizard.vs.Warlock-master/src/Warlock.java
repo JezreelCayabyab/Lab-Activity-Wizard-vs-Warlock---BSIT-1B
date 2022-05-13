606c68b448986471823a0421459c3c64d1f6b828
JezreelCayabyab <100850011+JezreelCayabyab@users.noreply.github.com>
2022-05-13 23:32:50 +0800

    Add files via upload

---
+++ Laboratory-Activity-Wizard.vs.Warlock-master/src/Warlock.java
@@ -0,0 +1,43 @@
+public class Warlock extends Character {
+    Warlock(String name) {
+        super(name);
+    }
+
+    public void NilheimMist(Character enemyCharacter) {
+        System.out.println(super.characterName + " attacks " + enemyCharacter.characterName
+                + " with MNilheim Mist (Damage - 40 , Mana Cost - 20)");
+        int damagePoints = 40;
+        damageTarget(enemyCharacter, damagePoints);
+        int manaPoints = 20;
+        manaTarget(enemyCharacter, manaPoints);
+
+    }
+
+    public void Ferocity(Character enemyCharacter) {
+        System.out.println(super.characterName + " attacks " + enemyCharacter.characterName
+                + " with Ferocity (Damage - 50 , Mana Cost - 30)");
+        int damagePoints = 50;
+        damageTarget(enemyCharacter, damagePoints);
+        int manaPoints = 30;
+        manaTarget(enemyCharacter, manaPoints);
+
+    }
+
+    public void DragonicFlame(Character characterName) {
+        System.out.println("Both use Heal");
+        System.out.println("~~~~~~~~~~~~~~~~~~");
+        System.out.println(super.characterName + " Use Dragonic Flame (Heal - 50)");
+        int healPoints = 50;
+        healTarget(characterName, healPoints);
+        manadTarget(characterName, manaPoints);
+    }
+
+    public void TartarusChains(Character enemyCharacter) {
+        System.out.println(super.characterName + " attacks " + enemyCharacter.characterName
+                + " with Tartarus Chains (Damage - 50 , Mana Cost - 15)");
+        int damagePoints = 50;
+        damageTarget(enemyCharacter, damagePoints);
+        int manaPoints = 15;
+        manaTarget(enemyCharacter, manaPoints);
+    }
+}
